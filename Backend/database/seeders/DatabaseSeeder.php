497073545037255a07696fad20b6fc5a3899ffe7
FlyingPeg <20520619@gm.uit.edu.vn>
2022-06-11 13:47:47 +0000

    seeder account

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::create([
+            'name' => 'FlyingPeg',
+            'email' => '20520619@gm.uit.edu.vn',
+            'password' => bcrypt('FnKL6upRv7uhvQ5Mj'),            
+        ]);
     }
 }
