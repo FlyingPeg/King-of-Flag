afd7e5064189e4492b94b1cd079d6b50fc3a752a
FlyingPeg <20520619@gm.uit.edu.vn>
2022-06-14 17:03:33 +0000

    route Room and Move

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\RoomController;
+use App\Http\Controllers\API\MoveController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +23,8 @@ Route::post('register', [AuthController::class, 'signup']);
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::resource('scores', UserController::class);
+    Route::resource('rooms', RoomController::class);
+    Route::resource('moves', MoveController::class);
     
     Route::get('/profile', [UserController::class, 'profile']);
     Route::put('/update-score', [UserController::class, 'score']);
