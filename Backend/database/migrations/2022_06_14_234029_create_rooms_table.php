df278565418cf0bc21c6d8391763ea0fe40c7df2
FlyingPeg <20520619@gm.uit.edu.vn>
2022-06-15 04:10:23 +0000

    step_b -> step

--- Backend/database/migrations/2022_06_14_234029_create_rooms_table.php
+++ Backend/database/migrations/2022_06_14_234029_create_rooms_table.php
@@ -18,7 +18,7 @@ class CreateRoomsTable extends Migration
             $table->string('room_id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->string('step_b')->nullable();
+            $table->string('step')->nullable();
             $table->timestamps();
         });
     }
