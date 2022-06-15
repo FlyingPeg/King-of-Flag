81e2101dccd83ec04b1cc399122c91c61a55cbb7
FlyingPeg <20520619@gm.uit.edu.vn>
2022-06-15 04:24:41 +0000

    return user_name

--- Backend/app/Http/Resources/Room.php
+++ Backend/app/Http/Resources/Room.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\User;
 
 class Room extends JsonResource
 {
@@ -14,9 +15,11 @@ class Room extends JsonResource
      */
     public function toArray($request)
     {
+        $user = DB::table('users')->where('id', $this->user_id)->first();
         return [
             'room_id' => $this->room_id,
             'user_id' => $this->user_id,
+            'user_name' => $user->name,
             'step' => $this->step,
         ];
     }
